92b7a89f846f72437088c179b27d8690f826f2cf
miyo-9 <https://github.com/miyo-9>
2025-12-11 10:20:50 +0900

    댓글기능구현3일차

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,18 @@
 import 'dotenv/config';
 
+<<<<<<< HEAD
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
     seed: "node ./prisma/db/seed/seed.js",
+=======
+export default {
+  datasources: {
+    db: {
+      adapter: 'postgresql',
+      url: process.env.DATABASE_URL,
+    },
+  },
+};
+>>>>>>> a37799b (댓글기능구현3일차)
