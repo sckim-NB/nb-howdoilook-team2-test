4bc7bb6511c950b2ebdc967cceafba0cd819d1b0
sckim-NB <sckim9654@gmail.com>
2025-12-11 04:12:26 +0000

    fix:sckim/curation schema 모델 추가

--- prisma/prisma.js
+++ prisma/prisma.js
@@ -1,12 +1,29 @@
-import { PrismaClient } from "@prisma/client";
-import "dotenv/config";
-import pkg from "pg";
-import { PrismaPg } from "@prisma/adapter-pg";
+// import "dotenv/config";
+// import pgPkg from "pg";
+// import { PrismaPg } from "@prisma/adapter-pg";
+// const { PrismaClient } = await import("@prisma/client");
 
-const { Pool } = pkg;
+// const { Pool } = pgPkg;
+
+// const pool = new Pool({
+//    connectionString: process.env.DATABASE_URL,
+// });
+
+// const adapter = new PrismaPg(pool);
+
+// const prisma = new PrismaClient({ adapter });
+
+// export default prisma;
+import "dotenv/config";
+import pgPkg from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+// 이 부분을 동적 import로 수정합니다.
+const { PrismaClient } = await import("@prisma/client");
+
+const { Pool } = pgPkg;
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+   connectionString: process.env.DATABASE_URL,
 });
 
 const adapter = new PrismaPg(pool);
