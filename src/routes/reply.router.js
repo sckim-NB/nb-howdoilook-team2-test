0a2798a6f180643687de92524847b5216f7cb961
miyo-9 <https://github.com/miyo-9>
2025-12-12 18:01:50 +0900

    Merge 전 변경사항 저장

--- src/routes/reply.router.js
+++ src/routes/reply.router.js
@@ -4,19 +4,16 @@ import { ReplyController } from '../controllers/reply.controller.js';
 const router = express.Router();
 const replyController = new ReplyController();
 
-// 1. 댓글 등록 
 router.post(
   '/curations/:curationId/comments',
   replyController.createReply
 );
 
-// 2. 댓글 수정
 router.put(
   '/comments/:commentId',
   replyController.updateReply
 );
 
-// 3. 댓글 삭제 
 router.delete(
   '/comments/:commentId',
   replyController.deleteReply
