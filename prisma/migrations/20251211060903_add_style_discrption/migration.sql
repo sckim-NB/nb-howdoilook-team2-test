66cfabe562338e7b6a731875225f873b143adf43
sckim-NB <sckim9654@gmail.com>
2025-12-16 07:09:35 +0000

    fix: sckim/GET 큐레이션-출력되는 답글 형식 변경

--- prisma/migrations/20251211060903_add_style_discrption/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `descripthion` on the `Style` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Style" DROP COLUMN "descripthion",
-ADD COLUMN     "description" TEXT;
